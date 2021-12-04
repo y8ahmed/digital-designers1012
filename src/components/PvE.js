0849ea01394b2792ea2cfcc9dd347a695dbf9e3c
Harib Shahbaz <harib327@my.yorku.ca>
2021-12-03 22:19:09 -0500

    Added extra funtionality to player vs computer.

--- src/components/PvE.js
+++ src/components/PvE.js
@@ -1,54 +1,138 @@
 import React, { useMemo, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-export default function PvE({myChoice, score, setScore, level}) {
-    const [selected, setSelected] = useState("");
-    const [computedSelected, setComputedSelected] = useState("");
-    // console.log(level)
+export default function PvE({ myChoice, level }) {
+  const [selected, setSelected] = useState("");
+  const [computerSelected, setComputedSelected] = useState("");
 
-    const play = () => {
-        // call backend and get the difficulty
-        console.log(level)
-        if (!selected) {
-            return;
-        }
-        if (level === 0) {
-            axios.get(`/easy`).then(res => {
-                setComputedSelected(res.data.move);
-            })  
-        } else if (level === 1) {
-            axios.get(`/hard`, { params: { selected: selected }}).then(res => {
-                setComputedSelected(res.data.move);
-            }) 
-        }
-    };
+  let [score, setScore] = useState(0);
 
-    const result = useMemo(() => {
-      if (computedSelected === selected) {
-        return `it's a tie`;
-      } else {
-        if (
-          (computedSelected === "rock" && selected === "scissors") ||
-          (computedSelected === "paper" && selected === "rock") ||
-          (computedSelected === "scissors" && selected === "paper")
+  let [computerScore, setComputerScore] = useState(0);
+
+  let [playerIndicator, setPlayerIndicator] = useState(0);
+
+  let [computerIndicator, setComputerIndicator] = useState(0);
+
+  let [counter, setCounter] = useState(0);
+
+  const [round, setRound] = useState("");
+
+  // console.log(level)
+
+  const play = () => {
+    // call backend and get the difficulty
+    console.log(level);
+    if (!selected) {
+      return;
+    }
+    if (level === 0) {
+      axios.get(`/easy`).then((res) => {
+        setComputedSelected(res.data.move);
+      });
+    } else if (level === 1) {
+      axios.get(`/hard`, { params: { selected: selected } }).then((res) => {
+        setComputedSelected(res.data.move);
+      });
+    }
+  };
+
+  const result = useMemo(() => {
+    setPlayerIndicator(playerIndicator++);
+    setComputerIndicator(computerIndicator++);
+
+    if (playerIndicator !== counter && computerIndicator !== counter) {
+      if (score !== 3 && computerScore !== 3) {
+        if (selected === "rock" && computerSelected === "scissors") {
+          setScore(score + 1);
+          setCounter(counter + 1);
+          return "Player 1 Won";
+        } else if (selected === "rock" && computerSelected === "paper") {
+          setComputerScore(computerScore + 1);
+          setCounter(counter + 1);
+          return "Player 2 Won";
+        } else if (selected === "scissors" && computerSelected === "paper") {
+          setScore(score + 1);
+          setCounter(counter + 1);
+          return "Player 1 Won";
+        } else if (selected === "scissors" && computerSelected === "rock") {
+          setComputerScore(computerScore + 1);
+          setCounter(counter + 1);
+          return "Player 2 Won";
+        } else if (selected === "paper" && computerSelected === "rock") {
+          setScore(score + 1);
+          setCounter(counter + 1);
+          return "Player 1 Won";
+        } else if (selected === "paper" && computerSelected === "scissors") {
+          setComputerScore(computerScore + 1);
+          setCounter(counter + 1);
+          return "Player 2 Won";
+        } else if (
+          selected === computerSelected &&
+          selected !== "" &&
+          computerSelected !== ""
         ) {
-          return "computer won";
+          return "It was a tie.";
         }
-        return "player won";
+      } else {
+        return "Game Over.";
       }
-    }, [computedSelected, selected]);
+    }
+  }, [selected, computerSelected]);
 
-    return (
+  const rounds = useMemo(() => {
+    if (score !== 3 && computerScore !== 3) {
+      if (score > computerScore) {
+        setRound(
+          `Player is currently in the lead with ${score} wins. Player  has ${
+            3 - score
+          } rounds left to win!`
+        );
+      } else if (computerScore > score) {
+        setRound(
+          `The Computer is currently in the lead with ${computerScore} wins. The Computer has ${
+            3 - computerScore
+          } rounds left to win!`
+        );
+      } else if (
+        score === computerScore &&
+        score !== "" &&
+        computerScore !== ""
+      ) {
+        setRound("The Player is Currntly Tied with the Computer");
+      }
+    } else {
+      if (score === 3) {
+        setRound("The Player has won the game.");
+      } else if (computerScore === 3) {
+        setRound("The Computer has won the game");
+      }
+    }
+  }, [score, computerScore]);
+
+  const playAgain = () => {
+    setSelected("");
+    setComputedSelected("");
+    setCounter(0);
+  };
+
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => setSelected("rock")}>rock</button>
-          <button onClick={() => setSelected("paper")}>paper</button>
-          <button onClick={() => setSelected("scissors")}>scissors</button>
-        </div>
-        <button onClick={play}>play</button>
-        <p>your choice: {selected}</p>
-        <p>computer's choice: {computedSelected}</p>
-        <div>{result}</div>
+        <h1>
+          {" "}
+          Player Score: {score} Computer Score: {computerScore}
+          {}
+        </h1>
+        <button onClick={() => setSelected("rock")}>rock</button>
+        <button onClick={() => setSelected("paper")}>paper</button>
+        <button onClick={() => setSelected("scissors")}>scissors</button>
       </div>
-    );
+      <button onClick={play}>play</button>
+      <p>your choice: {selected}</p>
+      <p>computer's choice: {computerSelected}</p>
+      <div>{result}</div>
+      <p>{rounds}</p>
+      <button onClick={playAgain}>Play Again?</button>
+    </div>
+  );
 }
